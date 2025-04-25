7155b45cb5d6a32d0d6ac1d2ccb054e74864c471
iKyba <84002383+iKyba@users.noreply.github.com>
2025-04-25 08:26:06 -0300

    Update PilaPU.java

--- PilaPU.java
+++ PilaPU.java
@@ -27,8 +27,26 @@ public class PilaPU implements PilaTDA {
     public boolean PilaVacia() {
         return tope == null;
     }
-
+	
     public Turno Tope() {
         return tope.dato;
     }
+ public void Mostrar() {
+         PilaPU aux = new PilaPU();
+         aux.InicializarPila();
+ 
+         while (!this.PilaVacia()) {
+             Turno t = this.Tope();
+             System.out.println(t);
+             aux.Apilar(t);
+             this.Desapilar();
+         }
+ 
+         while (!aux.PilaVacia()) {
+             Turno t = aux.Tope();
+             this.Apilar(t);
+             aux.Desapilar();
+         }
+     }
+ }
 }
